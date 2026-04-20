88c11499071675cbeb6ceac2d78a0d0610b7afac
Edyane <edyanealves@gmail.com>
2026-04-19 21:33:13 -0300

    feat: adicionando funcionalidades para abrir e fechar o menu

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+/*=============== SHOW & CLOSE MENU ===============*/
+const navMenu = document.getElementById('nav-menu')
+      navToggle = document.getElementById('nav-toggle')
+      navClose = document.getElementById('nav-close')
+
+// Show Menu
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+//Hide Menu
+if(navClose) {
+    navClose.addEventListener('click', ()=> {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MOBILE MENU ===============*/
+
+
+/*=============== HOME SWIPER ===============*/
+
+
+/*=============== CHANGE HEADER STYLES ===============*/
+
+
+/*=============== PRODUCTS SWIPER ===============*/
+
+
+/*=============== NEW SWIPER ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
