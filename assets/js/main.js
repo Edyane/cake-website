43cea16bc2274fcf80a92082733dc90684d71e72
Edyane <edyanealves@gmail.com>
2026-04-25 16:16:46 -0300

    feat: mudando estilo do header via lógica

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,78 +1,82 @@
 /*=============== SHOW & CLOSE MENU ===============*/
-const navMenu = document.getElementById('nav-menu')
-      navToggle = document.getElementById('nav-toggle')
-      navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu");
+navToggle = document.getElementById("nav-toggle");
+navClose = document.getElementById("nav-close");
 
 // Show Menu
-if(navToggle) {
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu')
-    })
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
 
 //Hide Menu
-if(navClose) {
-    navClose.addEventListener('click', ()=> {
-        navMenu.classList.remove('show-menu')
-    })
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
 /*=============== REMOVE MOBILE MENU ===============*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
-const linkAction = () =>{
-   const navMenu = document.getElementById('nav-menu')
-   // When we click on each nav__link, we remove the show-menu class
-   navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE HEADER STYLES ===============*/
-const scrollHeader = () =>{
-   const header = document.getElementById('header')
-   // Add the .scroll-header class if the bottom scroll of the viewport is greater than 50
-   this.scrollY >= 50 ? header.classList.add('scroll-header') 
-                      : header.classList.remove('scroll-header')
-}
-window.addEventListener('scroll', scrollHeader)
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  // Add the .scroll-header class if the bottom scroll of the viewport is greater than 50
+  this.scrollY >= 50
+    ? header.classList.add("scroll-header")
+    : header.classList.remove("scroll-header");
+};
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== HOME SWIPER ===============*/
-const swiperHome = new Swiper('.home__swiper', {
-    loop: true,
-    grabCursor: true,
-    speed: 800,
-    effect: 'creative',
-    creativeEffect: {
-        prev: {
-            translate: ['-120%', 0, -500],
-            rotate: [0, 0, -45],
-            opacity: 0,
-        },
-        next: {
-            translate: ['120%', 0, -500],
-            rotate: [0, 0, 45],
-            opacity: 0,
-        },
+const swiperHome = new Swiper(".home__swiper", {
+  loop: true,
+  grabCursor: true,
+  speed: 800,
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      translate: ["-120%", 0, -500],
+      rotate: [0, 0, -45],
+      opacity: 0,
     },
-    autoplay: {
-        delay: 3000,
-        disableInteraction: false,
-    }
+    next: {
+      translate: ["120%", 0, -500],
+      rotate: [0, 0, 45],
+      opacity: 0,
+    },
+  },
+  autoplay: {
+    delay: 3000,
+    disableInteraction: false,
+  },
 });
 
 /*=============== CHANGE HEADER STYLES ===============*/
-
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  // Add the .scroll-header class if the bottom scroll of the viewport is greater than 50
+  this.scrollY >= 50
+    ? header.classList.add("scroll-header")
+    : header.classList.remove("scroll-header");
+};
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== PRODUCTS SWIPER ===============*/
 
-
 /*=============== NEW SWIPER ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
